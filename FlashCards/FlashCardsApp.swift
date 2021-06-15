167de80521b7c46298d5793991954938bb090a06
Emre Yasa <emreyasa86@gmail.com>
2021-06-15 15:31:52 -0700

    Initial Commit

---
+++ FlashCards/FlashCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashCardsApp.swift
+//  FlashCards
+//
+//  Created by Emre Yasa on 6/15/21.
+//
+
+import SwiftUI
+
+@main
+struct FlashCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
