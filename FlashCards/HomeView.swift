6be6c8f9ce852b0f3f043c94a708efe3480b607a
Emre Yasa <emreyasa86@gmail.com>
2021-06-17 08:12:11 -0700

    Complete walkthrough screens

---
+++ FlashCards/HomeView.swift
@@ -0,0 +1,22 @@
+//
+//  HomeView.swift
+//  FlashCards
+//
+//  Created by Emre Yasa on 6/15/21.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Welcome to Home!")
+            .font(.title)
+            .fontWeight(.heavy)
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
